38aeae2db237088cf742c9c616f07f701253de81
Dallibo <denismalyarenko1@gmail.com>
2023-06-02 22:42:10 +0300

    Hw

---
+++ Hw_js_3.js
@@ -0,0 +1,27 @@
+// let numb = +prompt("Введите число");
+// if (numb === 0) { 
+//     alert("Вы ввели ноль");
+// }
+// else
+// if (numb > 0) { 
+//     alert("Положительное число");
+// }
+// else
+// if (numb < 0){
+//     alert("Отрицательное число");
+// }
+
+// let age = +prompt("Введіть ваш вік");
+// const currentYear = 2023;
+// let result1 = currentYear - age;
+// if (result1 >= 0 && result1 <= 120) {
+//     alert("Ваш вік: " + result1);
+// } else {
+//     alert("Некоректний вік");
+// }
+
+// let numb1 = +prompt("Введите число");
+// let mod = Math.abs(numb1);
+// alert("Модуль числа: " + mod);
+
+
